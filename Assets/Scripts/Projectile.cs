495771e27da5e0bccf511cbcd9cbb086bf1429d1
ToasterBiome <baiomurang@gmail.com>
2021-10-03 15:39:00 -0500

    rocket

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,18 +1,48 @@
+using System.Data.Common;
+using System.ComponentModel.Design.Serialization;
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Projectile : MonoBehaviour
 {
+    [SerializeField] float damage;
+    [SerializeField] float damageDropoff;
+    [SerializeField] Vector3 velocity;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += velocity * Time.deltaTime;
     }
+
+    void Explode()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, damage);
+        foreach (var hitCollider in hitColliders)
+        {
+            IDamageable damagable = hitCollider.GetComponent<IDamageable>();
+            if (damagable != null)
+            {
+                damagable.Damage(damage);
+            }
+        }
+
+        Destroy(gameObject);
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Explode();
+    }
+
 }
